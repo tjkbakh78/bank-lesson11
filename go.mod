e8f62cb56aa8a96c235783c4a7799f5997c5da28
tjkbakh78 <tjkbakh78@gmail.com>
2020-09-27 17:18:08 +0500

    Начальная версия

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tjkbakh78/bank
+
+go 1.15
---
+++ pkg/bank/types/types.go
@@ -0,0 +1,33 @@
+package types
+
+//Money тип это деньги
+type Money int64
+ 
+//Currency тип валюты там наши или русские или всеми любимые америкосы
+type Currency string
+
+
+//Коды валют 
+const(
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+//PAN  пан?! Кажется это номер карты, если, конечно память не изменяет.
+type PAN string 
+
+//MinBalance это минимальный баланс на карте 
+type MinBalance Money
+
+//Card тип - структура 
+type Card struct{
+	ID 			int 
+	PAN 			PAN
+	Balance 			Money 
+	MinBalance		Money
+	Currency 		Currency
+	Color 			string
+	Name 			string 
+	Active 			bool
+}
